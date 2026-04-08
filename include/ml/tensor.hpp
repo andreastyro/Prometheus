349c6924f39e833609a5c346aadcb3d50787ace1
andreastyro <andreastyr@gmail.com>
2026-04-09 01:47:31 +0300

    Initial Commit

---
+++ include/ml/tensor.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <vector>
+#include <stdexcept>
+
+class Tensor {
+public:
+
+    std::vector <float> data;
+    std::vector <int> shape;
+
+    Tensor(std::vector<int> shape_){
+        shape = shape_;
+        
+        int total = 1;
+
+        for (int i : shape) 
+            total *= i;
+
+        data.resize(total, 0.0f); // Create empty Tensor of 0s with shape_
+
+    }
+
+    int num_el() const{
+        int total = 1;
+        for (int d : shape)
+            total *= d;
+
+        return total;
+    }
+
+    float get(int row, int col){
+        return data[row * shape[1] + col];
+    }
+
+    void set(int row, int col, float value){
+        data[row * shape[1] + col] = value;
+    }
+
+};
